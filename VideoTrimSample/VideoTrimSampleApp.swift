fc81ad9a9c0ee35de287e26235ec482898be39a6
Berkay Özdemir <berkayozdemirv2@gmail.com>
2024-08-29 23:43:45 +0300

    Initial Commit

---
+++ VideoTrimSample/VideoTrimSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoTrimSampleApp.swift
+//  VideoTrimSample
+//
+//  Created by Berkay Özdemir on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoTrimSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
